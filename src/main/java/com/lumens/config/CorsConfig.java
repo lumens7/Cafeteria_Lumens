c0ae197b7160fbc709c6fc271836a673a7fecb27
lumens <luisfelixnogueira@gmail.com>
2025-04-19 17:13:32 -0300

    Configuração yml finalizada

--- src/main/java/com/lumens/config/CorsConfig.java
+++ src/main/java/com/lumens/config/CorsConfig.java
@@ -8,10 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/v3/api-docs/**")
                 .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(false);
+                .allowedMethods("GET");
     }
 }
