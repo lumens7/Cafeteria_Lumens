07e661c404058ee3e573bf3caf8ffd84f47037cf
lumens <luisfelixnogueira@gmail.com>
2025-04-19 15:26:26 -0300

    arquivos restantes

---
+++ src/test/java/com/lumens/CafeteriaLumensApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lumens;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CafeteriaLumensApplicationTests {
+
+	@Test
+	public void testContextLoads() {
+		System.out.println("Teste executado com sucesso!");
+	}
+}
